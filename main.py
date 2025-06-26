f152b8eaf151aaebf0f3cd728d4d6d05ad993213
Ernesto Perez <epmgrams@gmail.com>
2025-06-26 09:53:19 -0600

    Agregando los archivos del ejemplo para los Dunder Methos

---
+++ main.py
@@ -0,0 +1,21 @@
+from producto import Producto
+
+producto1 = Producto("Mouse", 20)
+producto2 = Producto("Mouse Premium", 150)
+
+# Llamado a __str__
+print(producto1)
+print(producto2)
+
+# Llamado a __repr__
+print(repr(producto1))
+print(repr(producto2))
+
+print("\nComparaciones:")
+print(producto1 < producto2)
+
+print("\nSumas:")
+print(producto1 + producto2)
+
+print("\nLongitud del nombre:")
+print(len(producto2))
---
+++ producto.py
@@ -0,0 +1,22 @@
+class Producto:
+    def __init__(self, nombre, precio):
+        self.__nombre = nombre
+        self.__precio = precio
+
+    def __str__(self):
+        return f"{self.__nombre} cuesta ${self.__precio}"
+
+    def __repr__(self):
+        return f"Producto('{self.__nombre}', '{self.__precio}')"
+
+    def __lt__(self, other):
+        return self.__precio < other.__precio
+
+    def __gt__(self, other):
+        return self.__precio > other.__precio
+
+    def __add__(self, other):
+        return self.__precio + other.__precio
+
+    def __len__(self):
+        return len(self.__nombre)
